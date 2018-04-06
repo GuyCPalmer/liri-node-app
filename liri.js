e8aee06d62ff93c7515122ab2abd4b73ca5127e1
Guy Palmer <guypalmer@Guys-Air.attlocal.net>
2018-04-06 16:42:05 -0500

    it might work

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ let request = require("request");
 var nodeArgs = process.argv;
 var action = nodeArgs[2].toLowerCase();
 
+//movieThis
 function movieThis(action, nodeArgs) {
     //variable for movie name
     var movieName = "";
@@ -23,14 +24,14 @@ function movieThis(action, nodeArgs) {
 
 //run OMDB API request with movie title using API key
 var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=aa1abc76"
-    //* Title of the movie.
-    //* Year the movie came out.
-    //* IMDB Rating of the movie.
-    //* Rotten Tomatoes Rating of the movie.
-    //* Country where the movie was produced.
-    //* Language of the movie.
-    //* Plot of the movie.
-    //* Actors in the movie.
+//* Title of the movie.
+//* Year the movie came out.
+//* IMDB Rating of the movie.
+//* Rotten Tomatoes Rating of the movie.
+//* Country where the movie was produced.
+//* Language of the movie.
+//* Plot of the movie.
+//* Actors in the movie.
 request(queryURL, function (error, response, body) {
     if (!error && response.statusCode === 200) {
         var bodyData = JSON.parse(body);
@@ -40,7 +41,7 @@ request(queryURL, function (error, response, body) {
             console.log("Title: " + bodyData.imdbRating);
 
             var rottenTomatoes = "";
-            for (var i = 0; i < bodyData.Ratings.length; i++){
+            for (var i = 0; i < bodyData.Ratings.length; i++) {
                 if (bodyData.Ratings[i].Source == "Rotten Tomatoes") {
                     rottenTomatoes = bodyData.Ratings[i].Value;
                 }
@@ -55,20 +56,78 @@ request(queryURL, function (error, response, body) {
         }
     }
 });
+//movieThis
+
+//twitterThis
+function twitterThis() {
+    var twitter = require("twitter");
+    var client = new twitter({
+        consumer_key: keys.twitter.consumer_key,
+        consumer_secret: keys.twitter.consumer_secret,
+        access_token_key: keys.twitter.access_token_key,
+        access_token_secret: keys.twitter.access_token_secret
+    });
+    var params = {
+        name: 'GuyClayton9',
+        screen_name: 'GuyClayton9',
+        exclude_replies: false,
+        trim_user: true,
+        count: 10
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var n = i + 1;
+                console.log(n + '------');
+                console.log('created: ' + tweets[i].created_at);
+                console.log('text: ' + tweets[i].text);
+                console.log('-------');
+            }
+        }
+    });
+}
+//twitter this
 
 
+if (action == "do-what-it-says") {
 
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        } else {
+            var output = data.split(",");
+            var nodeArgs = [];
+            for (var i = 0; i < 3; i++) {
+                nodeArgs[i] = "";
+            }
+            var tempName = "";
+            for (var i = 0; i < output.length; i++) {
+                if (i == 0) {
+                    action = output[i].toLowerCase();
+                } else {
+                    if (i == 1) {
+                        tempName = output[i];
+                    } else {
+                        tempName += "," + output[i];
+                    }
+                }
+            }
+            var nameArray = tempName.split(" ");
+            for (var i = 0; i < nameArray.length; i++) {
+                nodeArgs.push(nameArray[i]);
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            if (action == "movie-this") {
+                movieThis(action, nodeArgs);
+            } else if (action == "my-tweets") {
+                twitterThis();
+            }
+        } 
+    });
+} else 
+    if (action == "movie-this") {
+        movieThis(action, nodeArgs);
+    } else if (action == "my-tweets") {
+    twitterThis();
+    }
